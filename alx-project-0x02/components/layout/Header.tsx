34270003a2b0cdb0ac35602883b58970a10e971d
Joel Ekeng <joelekeng23@gmail.com>
2025-01-09 20:49:14 +0000

    Posts page fixed

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -6,7 +6,7 @@ const Header: React.FC = () => {
     <div className="flex justify-center gap-4 items-center p-4 bg-gray-200">
       <Link href="/home" className="text-blue-500 hover:text-blue-700 rounded-md p-2">Home</Link>
       <Link href="/about" className="text-blue-500 hover:text-blue-700 rounded-md p-2">About</Link>
-      <Link href="/post" className="text-blue-500 hover:text-blue-700 rounded-md p-2">Post</Link>
+      <Link href="/posts" className="text-blue-500 hover:text-blue-700 rounded-md p-2">Post</Link>
       <Link href="/users" className="text-blue-500 hover:text-blue-700 rounded-md p-2">Users</Link>
     </div>
   );
